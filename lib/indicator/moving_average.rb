e19addcd74a0b8669a4ed34d30818ed1cded79f9
saraki <saraki@step.dogteacher.com>
2015-10-10 16:31:10 +0900

    todo moving_average.rb

---
+++ lib/indicator/moving_average.rb
@@ -0,0 +1,14 @@
+require "./lib/base"
+
+#移動平均クラス
+#始値、高値、安値、終値
+#(:open :high :low :close ) のうちどれでも
+#:price_atパラメーターを指定しなければ、終値で計算する
+class MovingAverage < Indicator
+	def initialize(stock , params )
+		@stock = stock
+		@span = params[:span]
+		@price_at = params[:price_at] || :close
+	end
+
+	def calculat
